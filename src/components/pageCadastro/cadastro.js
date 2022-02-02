066e259e93e598d696664878d8bae841c182b0a2
Antonio <antonio.garciamn@gmail.com>
2022-02-02 15:21:16 -0300

    feat: sign-up users

--- src/components/pageCadastro/cadastro.js
+++ src/components/pageCadastro/cadastro.js
@@ -1,16 +1,64 @@
 import MainStyle from "./styleCadastro";
 import logo from "../imgs/logo.png";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function TelaCadastro() {
+	const [user, setUser] = useState({
+		name: "",
+		email: "",
+		password: "",
+	});
+	const [confirPassword, setConfPassword] = useState("");
+	const navegar = useNavigate();
+
+	async function cadastrar(e) {
+		e.preventDefault();
+
+		if (user.password != confirPassword) {
+			return alert("As senhas tem que ser iguais");
+		}
+
+		try {
+			await axios.post("http://localhost:5000/sign-up", user);
+			navegar("/");
+		} catch (error) {
+			console.log(error);
+			alert("Erro no Cadastro. Tente Novamente");
+		}
+		console.log(user);
+	}
+
 	return (
 		<MainStyle>
 			<img src={logo} alt="erro no logo" />
-			<form>
-				<input type="text" placeholder="Nome" />
-				<input type="email" placeholder="E-mail" />
-				<input type="password" placeholder="Senha" />
-				<input type="password" placeholder="Confirme a senha" />
+			<form onSubmit={cadastrar}>
+				<input
+					type="text"
+					value={user.name}
+					placeholder="Nome"
+					onChange={(e) => setUser({ ...user, name: e.target.value })}
+				/>
+				<input
+					type="email"
+					placeholder="E-mail"
+					value={user.email}
+					onChange={(e) => setUser({ ...user, email: e.target.value })}
+				/>
+				<input
+					type="password"
+					placeholder="Senha"
+					value={user.password}
+					onChange={(e) => setUser({ ...user, password: e.target.value })}
+				/>
+				<input
+					type="password"
+					placeholder="Confirme a senha"
+					value={confirPassword}
+					onChange={(e) => setConfPassword(e.target.value)}
+				/>
 				<input type="submit" value="Cadastra" />
 			</form>
 			<Link to="/">
